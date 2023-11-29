5a2851bd6ddbc52704e9d5f78486a4b464f70ddf
Maritsu <66056347+Maritsu@users.noreply.github.com>
2023-11-29 18:50:27 +0100

    [yes] once more

--- src/sources.hpp
+++ src/sources.hpp
@@ -14,8 +14,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <sources/scrape.hpp>
+#pragma once
 
 namespace libosm {
-
 }
